e1b2535182fcc20df5974c3372eede9475a64ff8
Pallav <pallavmishra314@gmail.com>
2024-05-03 17:25:04 +0530

    resolved git issues

--- vendorMan/vendorMan/urls.py
+++ vendorMan/vendorMan/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    re_path(r'^$', schema_view),
     path('admin/', admin.site.urls),
+    path('api/', include('vendorManagement.urls')),
 ]
