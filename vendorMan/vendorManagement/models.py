b4a2940080a643e11dae4c2861afca49de086b83
shauryasinghdev <sinngh.shaurya@gmail.com>
2024-05-07 15:13:53 +0530

    added test cases

--- vendorMan/vendorManagement/models.py
+++ vendorMan/vendorManagement/models.py
@@ -8,12 +8,12 @@ from datetime import datetime
 # Create your models here.
 class Vendor(models.Model):
     name = models.CharField(max_length=255)
-    contact_details = models.TextField(blank=True)
-    address = models.TextField(blank=True)
+    contact_details = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     vendor_code = models.CharField(unique=True, max_length=255)
-    on_time_delivery_rate = models.FloatField(blank=True)
-    average_response_time = models.FloatField(blank=True)
-    fulfillment_rate = models.FloatField(blank=True)
+    on_time_delivery_rate = models.FloatField(blank=True, null=True)
+    average_response_time = models.FloatField(blank=True, null=True)
+    fulfillment_rate = models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return self.name
